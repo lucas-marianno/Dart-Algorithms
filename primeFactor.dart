27d6b7bf06218e58299a12951615f8ccc025792a
LuccaMoraes <lucas.pk16@outlook.com>
2023-02-13 23:40:00 -0300

    prime factors complete

--- primeFactor.dart
+++ primeFactor.dart
@@ -1,29 +1,57 @@
 void main(){
 
   // https://www.youtube.com/watch?v=o35evzk02ug
-  print(3);
+
+  List<Set<int>> superset = [
+    primeFactors(24),
+    primeFactors(12),
+    primeFactors(18),
+    primeFactors(6),
+  ];
+
+  //print(superset);
+
+  print(commonFactor(superset));
 
 }
 
-List<int> primeFactors(int num){
-
-  if(num == 1) return [1];
-
+Set<int> primeFactors(int num){
+  if(num == 1) return {1};
   int n = 2;
-
-  List<int> factors = [];
-
+  Set<int> factors = {};
   while(n*n <= num){
     if( num % n == 0){
       factors.add(n);
-      num = num~/n;
+      num = num ~/ n;
     }else{
       n++;
     }
-    if(num > 1){
+  } 
+  if(num > 1){
       factors.add(num);
+  }
+  return factors;
+}
+
+int commonFactor(List<Set<int>> superset){
+  
+  List<int> common = superset[0].toList();
+
+  for(Set<int> set in superset){
+    for(int num in superset[0]){
+      if(!set.contains(num)){
+        common.remove(num);
+      }
     }
   }
-
-  return factors;
+  
+  if(common.length >1){
+    List commonInOrder = common.toList();
+    commonInOrder.sort();
+    return commonInOrder.last;
+  }else if(common.length == 1){
+    return common.toList()[0];
+  }else{
+    return 1;
+  }
 }
